8a9a3a7288716a6fc886613cbbe734cb7c10e89f
juanchete <juanlopezlmg@gmail.com>
2024-01-15 00:54:55 -0400

    Update API endpoint for watchlist data retrieval

--- source/features/home/services/getWatchlist.ts
+++ source/features/home/services/getWatchlist.ts
@@ -3,7 +3,7 @@ import {useQuery} from 'react-query';
 
 const fetchWatchlistData = async (adjusted: boolean, date: string) => {
   const response = await fetch(
-    `http://localhost:3000/api/endpoint1/watchlist?date=${date}&adjusted=${adjusted}`,
+    `http://localhost:3000/api/stocks/watchlist?date=${date}&adjusted=${adjusted}`,
   );
   if (!response.ok) {
     throw new Error('Error al obtener los datos de la watchlist');
